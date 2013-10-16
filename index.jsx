4778f4fe0191cc18688870840f2ba69f7824ad2c
Andrey Popp <8mayday@gmail.com>
2013-10-17 03:02:31 +0400

    better demo data

--- index.jsx
+++ index.jsx
@@ -6,8 +6,10 @@ var React     = require('react-tools/build/modules/React'),
 
 var doc = {
   blocks: [
-    {type: 'heading', level: 1, content: 'This is a title'},
-    {type: 'paragraph', content: 'Hello, world'},
+    {type: 'heading', level: 1, content: 'markstruct'},
+    {type: 'paragraph', content: 'Structured editor for markdown.'},
+    {type: 'heading', level: 2, content: 'Motivation'},
+    {type: 'paragraph', content: 'Phasellus pellentesque sed nisi nec lobortis. Suspendisse vestibulum pellentesque viverra. Aliquam erat volutpat. Proin laoreet, erat laoreet dignissim varius, metus ipsum pretium lorem, a aliquet risus ipsum in ante. Ut tempus augue et orci semper, eget lacinia orci fermentum. Proin iaculis, ipsum non eleifend rutrum, arcu lacus rhoncus dui, ac faucibus justo tellus eget tortor. Nulla imperdiet nisi in elementum malesuada. Ut ullamcorper augue turpis, ac scelerisque est scelerisque non. Proin eu libero est.'}
   ]
 }
 
