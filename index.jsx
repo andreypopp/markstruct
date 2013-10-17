036d6e67bb99e7d8368b9a53fdd75faf02203a3b
Andrey Popp <8mayday@gmail.com>
2013-10-17 05:38:33 +0400

    better styles

--- index.jsx
+++ index.jsx
@@ -8,6 +8,8 @@ var doc = {
   blocks: [
     {type: 'heading', level: 1, content: 'markstruct'},
     {type: 'listitem', content: 'Structured editor for markdown.'},
+    {type: 'listitem', content: 'Structured editor for markdown.'},
+    {type: 'listitem', content: 'Structured editor for markdown.'},
     {type: 'heading', level: 2, content: 'Motivation'},
     {type: 'paragraph', content: 'Phasellus pellentesque sed nisi nec lobortis. Suspendisse vestibulum pellentesque viverra. *Aliquam* erat volutpat. Proin laoreet, erat laoreet dignissim varius, metus ipsum pretium lorem, a aliquet risus ipsum in ante. Ut tempus augue et orci semper, eget lacinia orci fermentum. Proin iaculis, ipsum non eleifend rutrum, arcu lacus rhoncus dui, ac faucibus justo tellus eget tortor. Nulla imperdiet nisi in elementum malesuada. Ut ullamcorper augue turpis, ac scelerisque est scelerisque non. Proin eu libero est.'}
   ]
