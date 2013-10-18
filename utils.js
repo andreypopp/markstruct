27b5a911894dbc5cd89151a94e0e7efee0078c58
Andrey Popp <8mayday@gmail.com>
2013-10-18 13:44:38 +0400

    text range utils

---
+++ utils.js
@@ -0,0 +1,66 @@
+require('rangy-browser/lib/rangy-core');
+
+function rangeHeight(node, start, end) {
+  var rng = rangy.createRange();
+  rng.setStart(node, start);
+  rng.setEnd(node, end);
+  return rng.nativeRange.getBoundingClientRect().height;
+}
+
+function computeLineMetrics(node) {
+  var rng,
+      fix = 1,
+      cur = getSelection().getRangeAt(0),
+      outerRect = node.getBoundingClientRect();
+
+  // TODO: what about ranges which are not collapsed?
+  // TODO: what about node with more than a single children?
+
+  if (!node.firstChild)
+    return {line: 1, totalLines: 1};
+
+  // compute first line height
+  var text = node.firstChild;
+  var firstLineHeight = rangeHeight(text, 0, 1);
+  var lastLineHeight = rangeHeight(text, 0, text.wholeText.length);
+  var currentLineHeight = rangeHeight(text, 0, Math.max(cur.startOffset, 1));
+
+  // compute delta height and offset from start of the line
+  var deltaHeight = 1,
+      lineOffset = cur.startOffset;
+
+  if (firstLineHeight === currentLineHeight) {
+    for (var offset = text.wholeText.length - 1; offset > 0; offset--) {
+      var height = rangeHeight(text, 0, offset);
+      if (height !== lastLineHeight) {
+        deltaHeight = lastLineHeight - height;
+        break;
+      }
+    }
+  } else {
+    for (var offset = cur.startOffset - 1; offset > 0; offset--) {
+      var height = rangeHeight(text, 0, offset);
+      if (height !== currentLineHeight) {
+        deltaHeight = currentLineHeight - height;
+        lineOffset = cur.startOffset - offset;
+        break;
+      }
+    }
+  }
+
+  if (currentLineHeight != rangeHeight(text, 0, cur.startOffset + 1)) {
+    fix = 2;
+    lineOffset = 0;
+  }
+
+  return {
+    line: fix + (currentLineHeight - firstLineHeight) / deltaHeight,
+    lineOffset: lineOffset,
+    offset: cur.startOffset,
+    totalLines: 1 + (lastLineHeight - firstLineHeight) / deltaHeight
+  }
+}
+
+module.exports = {
+  computeLineMetrics: computeLineMetrics
+};
