6dba498c5faafb09b59d65c0cd105b2a71c39405
Andrey Popp <8mayday@gmail.com>
2013-10-20 14:20:28 +0400

    serializer

---
+++ serializer.js
@@ -0,0 +1,29 @@
+var markdown = require('markdown').markdown;
+
+function toAst(doc) {
+  var ast = ['markdown'];
+  doc.forEach(function(block) {
+    switch(block.type) {
+      case 'paragraph':
+        ast.push(['para', block.content]);
+        break;
+      case 'heading':
+        ast.push(['header', {level: block.level}, block.content]);
+        break;
+      case 'code':
+        ast.push(['code', block.content]);
+        break;
+      case 'listitem':
+        ast.push(['listitem', block.content]);
+        break;
+      default:
+        ast.push(['para', block.content || '']);
+    }
+  });
+  return ast;
+}
+
+module.exports = function(doc) {
+  var ast = toAst(doc);
+  return markdown.renderJsonML(markdown.toHTMLTree(ast));
+}
