81c7cba9d01f795276fc51fad7f19c690810357f
Andrey Popp <8mayday@gmail.com>
2013-10-20 14:14:47 +0400

    parse from markdown into doc

---
+++ parser.js
@@ -0,0 +1,49 @@
+var markdown = require('markdown').markdown;
+
+function flatten(ast) {
+  return ast
+    .map(function(n) { return Array.isArray(n) ? n.slice(1) : n })
+    .reduce(function(a, b) { return a.concat(b); }, [])
+    .join('');
+}
+
+function parse(src) {
+
+}
+
+function walk(ast) {
+  var doc = [];
+
+  ast.slice(1).forEach(function(block) {
+    switch(block[0]) {
+      case 'header':
+        doc.push({
+          type: 'heading',
+          level: block[1].level,
+          content: flatten(block.slice(2))
+        });
+        break;
+      case 'para':
+        doc.push({type: 'paragraph', content: flatten(block.slice(1))});
+        break;
+      case 'bulletlist':
+        doc = doc.concat(walk(block));
+        break;
+      case 'listitem':
+        doc.push({type: 'listitem', content: flatten(block.slice(1))});
+        break;
+      case 'code_block':
+        doc.push({type: 'code', content: flatten(block.slice(1))});
+        break;
+      default:
+        doc.push({type: 'paragraph', content: flatten(block.slice(1))});
+    }
+  });
+
+  return doc;
+}
+
+module.exports = function(src) {
+  var ast = markdown.parse(src);
+  return walk(ast);
+}
